e128c604a7754a488db3f32df763d812d54321aa
blueMedium <matrakin1@yahoo.com>
2020-05-10 12:22:14 +1000

    Add files via upload

---
+++ build/precache-manifest.1bcb96f7de9379a4e791c2d4227cf895.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d10d569c61a40c1d2694e8172606cc",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b6f133f08116f53c2240",
+    "url": "/robofriends/static/css/2.1544df54.chunk.css"
+  },
+  {
+    "revision": "b0a5e4c717a926493228",
+    "url": "/robofriends/static/css/main.020b29cc.chunk.css"
+  },
+  {
+    "revision": "b6f133f08116f53c2240",
+    "url": "/robofriends/static/js/2.d82cd422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.d82cd422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0a5e4c717a926493228",
+    "url": "/robofriends/static/js/main.2c811393.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
---
+++ src/robots.js
@@ -0,0 +1,40 @@
+export const robots = [
+
+{
+    id: 1,
+    name: 'Luke Skywalker',
+    username: 'Jedi',
+    email: 'luke@rebellion.com'
+},
+{
+    id: 2,
+    name: 'Darth Vader',
+    username: 'darkside',
+    email: 'darth@mpire.com'
+},
+{
+    id: 3,
+    name: 'Han Solo',
+    username: 'Smuggler',
+    email: 'han@solo.com'
+},
+{
+    id:4,
+    name: 'Princess Leia',
+    username: 'General',
+    email: 'leia@rebellion.com'
+},
+{
+    id:5,
+    name: 'R2D2',
+    username: 'robot',
+    email: 'r2d2@rebellion.com'
+},
+{
+    id:6,
+    name: 'Chewbacca',
+    username: 'Chewie',
+    email: 'chew@rebellion.com'
+}
+
+]
